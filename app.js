8ce248bd2bddc2a6dd4407fd399f979cc3eb6476
marialuizavieira20 <marialuizavieiradasilva78@gmail.com>
2026-05-26 23:34:27 -0300

    Mudando Dashboard

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ var indexRouter = require("./src/routes/index");
 var usuarioRouter = require("./src/routes/usuarios");
 var dashboardRouter = require("./src/routes/dashboard");
 var quizRouter = require("./src/routes/quiz");
+var avisosRouter = require("./src/routes/avisos");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -30,6 +31,7 @@ app.use("/", indexRouter);
 app.use("/usuarios", usuarioRouter);
 app.use("/dashboard", dashboardRouter);
 app.use("/quiz", quizRouter);
+app.use("/avisos", avisosRouter);
 
 app.listen(PORTA_APP, function () {
     console.log(`
